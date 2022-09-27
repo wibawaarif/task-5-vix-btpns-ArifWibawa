e7b0f6a2c44110390d9294da6771e68b65ddc7f4
Arif Wibawa <105232319+wibawaarif@users.noreply.github.com>
2022-09-28 00:55:05 +0700

    Update user.go

--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	Id uint32 					`gorm:"primaryKey" json:"id"`
-	Username string 		`gorm:"varchar(300)" json:"username"`
-	Email string 				`gorm:"varchar(300)" json:"email"`
-	Password string 		`gorm:"varchar(300)" json:"password"`
+	Id uint32 `gorm:"primaryKey" json:"id"`
+	Username string `gorm:"varchar(300)" json:"username"`
+	Email string `gorm:"varchar(300)" json:"email"`
+	Password string `gorm:"varchar(300)" json:"password"`
 	CreatedAt time.Time `gorm:"type:time" json:"createdat"`
   UpdatedAt time.Time `gorm:"type:time" json:"updatedat"`
 }
